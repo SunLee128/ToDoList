b5903c26a6ed2b254e4543158818f16a051db059
sunlee <sunlee128@gmail.com>
2019-12-15 20:06:42 +1100

    mongodb port updated

--- models/index.js
+++ models/index.js
@@ -1,7 +1,10 @@
 
+require('dotenv').config()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/todo-api', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-api',{useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/todo-api', {useNewUrlParser: true});
 mongoose.Promise = Promise;
 
 module.exports.Todo = require('./todo');
 
+mongodb+srv://Sun:<password>@todolist-samxl.mongodb.net/test?retryWrites=true&w=majority

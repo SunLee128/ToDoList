096d7b56b78c144a67bd325926df5a8a25b6b4f9
sunlee <sunlee128@gmail.com>
2019-12-16 11:43:25 +1100

    mongodb url to mongodb uri

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 
 require('dotenv').config()
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/todo-api',{useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todo-api',{useNewUrlParser: true});
 // mongoose.connect('mongodb://localhost:27017/todo-api', {useNewUrlParser: true});
 mongoose.Promise = Promise;
 
